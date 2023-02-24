120ef7c82ce5759df18d97584a2f4d13b181975f
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-24 16:59:50 +0530

    feat: initial setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h2 className="text-blue-600 font-mono text-5xl">TailwindCSS Is Working!</h2>
+    </div>
+  )
+}
