faebddaff8e457328514b5b6cc742b8049eff5fe
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-25 22:22:09 +0530

    feat: create a route for the login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,7 @@
+export default function login() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
+    </div>
+  )
+}
